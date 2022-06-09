ff47ab2566eb0b5e11b5c316956d0319e6130336
Douglas Obara <douglasobara97@gmail.com>
2022-06-09 13:44:33 +0300

    commit tree 3bc3740ce5e24d89166800bbbafb6edeed452212

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(task = "roller-skate") {
+  return "This Saturday, I want to" + ` ${task}!`;
+}
+
+let mondayWork = function (toDo = "go to the office") {
+  return `This Monday, I will ${toDo}.`;
+};
+
+let wrapAdjective = function (name = "*") {
+  return function (adj = "exceptional") {
+    return `You are ${name}${adj}${name}!`;
+  };
+};
